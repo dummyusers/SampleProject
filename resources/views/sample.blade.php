04a6885665f3a294528b42db34820ec82e3ace55
rollex.garcia <rollex.garcia@gmail.com>
2017-06-20 13:22:36 +0800

    angular.min.js

---
+++ resources/views/sample.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en" ng-app="justSample">
+<head>
+    <meta charset="utf-8">
+    <title>My HTML File</title>
+
+</head>
+<body>
+
+<p>Nothing here << 'yet' + '!' >></p>
+<script src="/js/angular.min.js"></script>
+<script src="/js/app.js"></script>
+<script src="/js/sample.js"></script>
+</body>
+</html>
