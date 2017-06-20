46f28f7142389f6d56532d9cb0a0cce5bde340cb
rollex.garcia <rollex.garcia@gmail.com>
2017-06-20 13:33:40 +0800

    enviromentationalitiesed

--- resources/views/sample.blade.php
+++ resources/views/sample.blade.php
@@ -3,12 +3,11 @@
 <head>
     <meta charset="utf-8">
     <title>My HTML File</title>
+    <script src="/js/angular.min.js"></script>
 
 </head>
 <body>
-
 <p>Nothing here << 'yet' + '!' >></p>
-<script src="/js/angular.min.js"></script>
 <script src="/js/app.js"></script>
 <script src="/js/sample.js"></script>
 </body>
